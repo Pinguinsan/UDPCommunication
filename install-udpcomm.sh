4963b5a730826a8de528ca4046bc0f0bcc2b3c68
Tyler Lewis <Pinguinsan@gmail.com>
2016-12-18 07:44:00 -0500

    Added better install script - Latitude-5559-PC

--- install-udpcomm.sh
+++ install-udpcomm.sh
@@ -1,32 +1,215 @@
-
 #!/bin/bash
 
-iconName="doomlauncher.png"
-skeletonDesktopFileName=".doomlauncher.desktop.skel"
-desktopFileName="doomlauncher.desktop"
-programName="udpcomm"
+##########################################
+# install-udpcomm.sh
+#
+# Intended to be a placeholder until 
+# I learn how to add a sudo make 
+# install/uninstall to CMake
+#
+##########################################
 
+programName="udpcomm"
+programLongName="UDP Communication"
 absolutePath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 fileName="${absolutePath##*/}"
-filePath="$(dirname "$absolutePath")/"
+filePath="$(dirname "$absolutePath")"
 sourceDir="$filePath/src/"
 globalBinDir="/usr/bin"
-#echo "$absolutePath"
-#echo "$fileName"
-#echo "$filePath"
-
+dependancyLibName="lib$dependancyRootName.so"
+libDir="/usr/lib/"
+dependancy="$libDir/$dependancyLibName"
+dependancyRootName="tjlutils"
+dependancySource="https://github.com/Pinguinsan/$dependancyRootName"
+dependancyInstallCommand="$dependancyRootName-install.sh"
 buildDir="build"
+
+function displayHelp() {
+    echo "Usage: install-$programName.sh [--install/--uninstall] [build-dir]"
+}
+
+cygwinCheck=$(uname -a | grep -i 'cygwin')
+
+if [[ -z "$cygwinCheck" ]]; then
+   if [[ "$EUID" != "0" ]]; then
+      SUDO="sudo"
+   fi
+else
+    SUDO=""
+fi
+
+function doInternetCheck() {
+    echo -n "Checking for internet connection...."
+    internetCheck=$(ping 8.8.8.8 -c1 -W1 | grep '1 received')
+    if [[ -z "$internetCheck" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function cleanUp() {
+    echo "All cleaned up"
+}
+
+function showSuccess() {
+    echo "success"
+}
+
+function showFailure() {
+    echo "failure"
+    cleanUp
+}
+
+function removeFile() {
+    echo -n "Removing \"$1\"..."
+    rm -f "$1"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function linkFile() {
+    echo -n "Linking \"$1\" to \"$2\"..."
+    ln -s -f "$1" "$2"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function copyFile() {
+    echo -n "Copying \"$1\" to \"$2\"..."
+    cp -R "$1" "$2"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function appendStringToFile() {
+    echo -n "Putting string \"$1\" into file $2..."
+    echo "$1" >> "$2"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function changeDirectory() {
+    echo -n "Entering directory \"$1\"..."
+    cd "$1"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function createDirectory() {
+    echo -n "Creating directory \"$1\"..."
+    mkdir "$1"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function runGitClone() {
+    echo -n "Cloning \"$1\" using git..."
+    git clone "$1"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function runGitPullOrigin() {
+    echo -n "Fetching source for \"$1\" using git..."
+    git pull origin master
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function runCmake() {
+    echo -n "Running cmake from source directory \"$1\"..."
+    cmake "$1"
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
+
+function runMake() {
+    echo -n "Running make..."
+    make
+    if [[ "$?" -ne "0" ]]; then
+        showFailure
+        return 1
+    else 
+        showSuccess
+        return 0
+    fi
+}
 
 function bailout() {
     rm -rf "$buildDir"
 }
 
-function generateDesktopFile() {
-    cp "$utilityDir/$skeletonDesktopFileName" "$buildDir/$desktopFileName" || { echo "Failed to generate desktop file, exiting"; exit 1; }
-    cp "$iconPath" "$buildDir/"
-    echo "Exec=$buildDir$programName" >> "$buildDir/$desktopFileName"
-    echo "Icon=$buildDir$iconName" >> "$buildDir/$desktopFileName"
+function retrieveDependancy() {
+    dependancyDir="$buildDir/lib"
+    
+    if ! [[ -d "$dependancyDir" ]]; then
+        echo "$dependancyDir does NOT exist"
+        createDirectory "$dependancyDir" || { echo "Failed to make dependancy lib directory, bailing out"; exit 1; }
+    fi
+    
+    if ! [[ -d "$dependancyDir/$dependancyRootName" ]]; then
+        changeDirectory "$dependancyDir"  || { echo "Failed to enter dependancy lib directory, bailing out"; exit 1; }
+        runGitClone "$dependancySource" || { echo "Failed to retrieve dependancy source, bailing out"; exit 1; }
+        changeDirectory "$depenancyDir/$dependancyRootName" || { echo "Failed to enter dependancy lib directory, bailing out"; exit 1; }
+    else
+        changeDirectory "$dependancyDir/$dependancyRootName"  || { echo "Failed to enter dependancy lib directory, bailing out"; exit 1; }
+        runGitPullOrigin "$dependancySource" || { echo "Failed to retrieve dependancy source, bailing out"; exit 1; }
+    fi
 }
+
+function buildDependancy() {
+     source "$dependancyDir/$dependancyRootName/$dependancyInstallCommand" "$buildDir" || { echo "Failed to build dependancy, bailing out"; exit 1; }
+}
+
+buildDir="build"
 
 trap bailout INT QUIT TERM
 if [[ $# -gt 0 ]]; then
@@ -36,25 +219,31 @@ if [[ $# -gt 0 ]]; then
     fi
     buildDir="$1"
     if ! [[ -d "$buildDir" ]]; then
-        mkdir "$buildDir" || { echo "Unable to create build directory \"$buildDir\", exiting"; exit 1; }
+        createDirectory "$buildDir" || { echo "Unable to create build directory \"$buildDir\", exiting"; exit 1; }
     fi
 else
     buildDir="$filePath/$buildDir"
     if ! [[ -d "$buildDir" ]]; then
-        mkdir "$buildDir" || { echo "Unable to make build directory \"$buildDir\", exiting"; exit 1; }
+        createDirectory "$buildDir" || { echo "Unable to make build directory \"$buildDir\", exiting"; exit 1; }
     fi
 fi
 
-cd "$buildDir" || { echo "Unable to enter build directory \"$buildDir\""; exit 1; }
-cmake "$filePath" || { echo "cmake failed, bailing out"; exit 1; }
-make || { echo "make failed, bailing out"; exit 1; }
+changeDirectory "$buildDir" || { echo "Unable to enter build directory \"$buildDir\""; exit 1; }
+dependancyResult=$(ls "$libDir" | grep "$dependancyRootName")
+if [[ -z "$dependancyResult" ]]; then
+    doInternetCheck || { echo "There is no active internet connection, so dependancy cannot be retrieved, bailing out"; exit 1; }
+    retrieveDependancy || { echo "Coult not retrieve dependancy source, bailing out"; exit 1; }
+    buildDependancy || { echo "Building dependancy failed, bailing out"; exit 1; }
+fi
+runCmake "$filePath" || { echo "cmake failed, bailing out"; exit 1; }
+runMake || { echo "make failed, bailing out"; exit 1; }
 
 
-sudo ln -s -f "$buildDir/$programName" "$globalBinDir"
+sudo ln -s -f "$buildDir/$programName" "$globalBinDir"  || { echo "Could not link file, bailing out"; exit 1; }
 
 echo
 echo "*********************************************"
-echo "**UDP Communication Installed Successfully!**"
+echo "**$programLongName Installed Successfully!**"
 echo "*********************************************"
 echo 
 exit 0
